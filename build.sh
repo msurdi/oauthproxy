8ef1cf8a4a22e9e9305e031773d101847b3aa589
Matias Surdi <matiassurdi@gmail.com>
2012-11-19 13:26:19 +0100

    Removed 386 builds

--- build.sh
+++ build.sh
@@ -2,10 +2,12 @@
 BUILD_DIR="build"
 DIST_DIR="dist"
 VERSION="0.1"
+ARCHS="amd64"
+OSS="linux darwin"
 mkdir -p $BUILD_DIR
 mkdir -p $DIST_DIR
-for os in "linux" "darwin";do
-	for arch in "amd64" "386";do
+for os in $OSS;do
+	for arch in $ARCHS;do
 		echo "building ${os}-${arch} binary..."
 		GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o $BUILD_DIR/oauthproxy-${os}-${arch}
 		echo "Creating package for ${os}-${arch}"
